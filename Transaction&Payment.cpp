ea195edeb4c478a7be1afeac97e51bd27cb7b111
Rajdeepsinh Pravinbhai Rathod <rajdeeprathod453@gmail.com>
2025-09-11 10:02:39 +0530

    Update Transaction&Payment.cpp

--- Transaction&Payment.cpp
+++ Transaction&Payment.cpp
@@ -1,15 +1,93 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 
 #define SUCCESS 1
 #define FAILURE 0
 
+using namespace std;
+class product;
+class product{
+ static int id;
+ string name;
+int price;
+string catogery;
+bool isavail;
+string descrip;
 
+public:
+
+product(){
+id = id+1;
+isavail=true;
+}
+product(int price, string name, string cat,string d){
+price=price;
+id=id+1;
+name=name;
+catogery=cat;
+descrip=d;
+isavail=true;
+}
+product(int price, string name, string cat){
+price=price;
+id=id+1;
+name=name;
+catogery=cat;
+descrip="description is not given";
+isavail=true;
+}
+
+}
+
+;
+class slot{
+    public:
+int slotid;
+int productid;
+int quantity;
+int capacity;
+void restock(int amount){
+    if(amount+quantiy >capacity){
+        cout<<"stock is  added and it is full"
+    }
+    quantity=max(quantity+amount,capacity);
+}
+bool dispense(int q){
+    if(q<=quantity) quantity -= q;
+    else {
+        cout<<"insufficient stock ,only"<<quantity<<"items left"<<endl;
+    }
+}
+
+
+
+
+};
+class transaction{
+    int txid;
+    int slotid;
+    int productid;
+    int quantity;
+    int pricepaid;
+    int changegiven;
+    bool status;
+     
+};
+class vendingmachine{
+
+};
+int product::id=0;
+int main(){
+  
+    return 0;
+}
 class Transaction;  
-
+class product;
 
 class VendingMachine {
 private:
@@ -35,12 +113,6 @@ public:
         price = vm.price;
     }
 
- 
-    ~VendingMachine() {
-        cout << "Destructor called for product: " << productName << endl;
-    }
-
-   
     static void showTransactionCount() {
         cout << "Total Transactions so far: " << transactionCount << endl;
     }
@@ -170,4 +242,5 @@ int main() {
     VendingMachine::showTransactionCount();
     return 0;
 }
+
 
