dbbb22bea21c8f92d6753054d0bab23bcc0ca279
rajdeep453 <rajdeeprathod453@gmail.com>
2025-09-17 23:58:52 +0530

    "update the vending machine "

--- vendingm.cpp
+++ vendingm.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
+#include<admin.h>
 #include<inventory.h>
+#include<product.h>
+#include<ipayment.h>
+#include<logger.h>
 #include<vmnchstate.h>
 
 using namespace std;
 class vendingm{
+
 vmstate currentstate;
+
 // inventory inv;
 public:
     
